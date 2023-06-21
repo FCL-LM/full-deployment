13bf15238307c1190eff08bfb3c64da44c7a2730
josura <giorgiolocicero@hotmail.com>
2023-06-21 21:30:45 +0200

    fixed seaweedfs deployment

--- k8s-deployment/apply-configuration.sh
+++ k8s-deployment/apply-configuration.sh
@@ -4,8 +4,16 @@ kubectl apply -f s3-config.yaml
 # deployments
 kubectl apply -f elastic-deployment.yaml
 kubectl apply -f seaweedfs-deployment.yaml
+# kubectl apply -f seaweedfs-master-deployment.yaml
+# kubectl apply -f seaweedfs-volume-deployment.yaml
+# kubectl apply -f seaweedfs-filer-deployment.yaml
+# kubectl apply -f seaweedfs-s3-deployment.yaml
+
 kubectl apply -f gnam-api-deployment.yaml
 kubectl apply -f privategpt-deployment.yaml
 kubectl apply -f vector-store-updater-deployment.yaml
-kubectl apply -f privategpt-loadbalancer-service.yaml
 kubectl apply -f web-gui-deployment.yaml
+
+# services
+kubectl apply -f privategpt-loadbalancer-service.yaml
+kubectl apply -f services.yaml
