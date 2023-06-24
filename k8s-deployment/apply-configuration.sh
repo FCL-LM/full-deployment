fef1ab7b204cc42e4d2099410c1791a4f84930fc
josura <giorgiolocicero@hotmail.com>
2023-06-25 00:41:31 +0200

    persistent volumes added to configuration script

--- k8s-deployment/apply-configuration.sh
+++ k8s-deployment/apply-configuration.sh
@@ -1,6 +1,12 @@
 # config maps
 kubectl apply -f s3-config.yaml
 
+# mount volumes
+kubectl apply -f seaweedfs-volume-persistentvolumeclaim.yaml
+kubectl apply -f seaweedfs-filer-persistentvolumeclaim.yaml
+kubectl apply -f seaweedfs-master-persistentvolumeclaim.yaml
+kubectl apply -f gnam-api-persistentvolumeclaim.yaml
+
 # deployments
 kubectl apply -f elastic-deployment.yaml
 #kubectl apply -f seaweedfs-deployment.yaml
@@ -10,10 +16,10 @@ kubectl apply -f seaweedfs-filer-deployment.yaml
 kubectl apply -f seaweedfs-s3-deployment.yaml
 
 kubectl apply -f gnam-api-deployment.yaml
-kubectl apply -f privategpt-deployment.yaml
-kubectl apply -f vector-store-updater-deployment.yaml
-kubectl apply -f web-gui-deployment.yaml
+#kubectl apply -f privategpt-deployment.yaml
+#kubectl apply -f vector-store-updater-deployment.yaml
+#kubectl apply -f web-gui-deployment.yaml
 
 # services
-kubectl apply -f privategpt-loadbalancer-service.yaml
+#kubectl apply -f privategpt-loadbalancer-service.yaml
 kubectl apply -f services.yaml
